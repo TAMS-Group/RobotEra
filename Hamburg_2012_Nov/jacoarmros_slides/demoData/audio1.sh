79529feb8277c8e1d419be110c42ec345620ac59
Sebastian <sebastian@rockel.cc>
2012-11-21 16:40:46 +0100

    fixed executable flag, added jaco arm ros slides

---
+++ Hamburg_2012_Nov/jacoarmros_slides/demoData/audio1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -f 
+mplayer -really-quiet /opt/kde3/share/sounds/KDE_Desktop1.wav
---
+++ Hamburg_2012_Nov/jacoarmros_slides/demoData/video1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -f 
+mplayer -really-quiet -ontop demoData/PA10-grasp.MPG 
+#xine --no-splash -l demoData/PA10-grasp.MPG 
+#xine --no-splash -pq demoData/PA10-grasp.MPG 
